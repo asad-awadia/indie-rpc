5d9f9d3c8b24051e007d28a212b0d5d1718c025f
Asad Awadia <6923282+asad-awadia@users.noreply.github.com>
2021-10-23 09:41:08 -0400

    better support for service dependencies (#1)

--- src/test/kotlin/dev/aawadia/RpcTest.kt
+++ src/test/kotlin/dev/aawadia/RpcTest.kt
@@ -9,25 +9,28 @@ class RpcTest {
   @Test
   fun testRpc() {
     val serviceName = "math.svc"
+    val const = 5
+    val x = 1
+    val y = 2
     val indieServer = IndieRpcServer()
-    indieServer.registerService(serviceName, MyFakeTestMathService::class)
+    indieServer.registerService(serviceName, MyFakeTestMathService(const))
     indieServer.startServer()
     val indieClient = IndieRpcClient()
     val addResponseResult = indieClient.invokeRpcMethod(
       RpcTarget(serviceName, "sum"),
-      MyFakeTestMathService.AddRequest(1, 2),
+      MyFakeTestMathService.AddRequest(x, y),
       MyFakeTestMathService.AddResponse::class.java
     )
     assert(addResponseResult.isSuccess)
-    assert(addResponseResult.getOrThrow().sum == 3)
+    assert(addResponseResult.getOrThrow().sum == (x + y + const))
   }
 }
 
-class MyFakeTestMathService {
+class MyFakeTestMathService(private val const: Int) {
   data class AddRequest(val x: Int, val y: Int)
   data class AddResponse(val sum: Int)
 
   fun sum(addRequest: AddRequest): AddResponse {
-    return AddResponse(addRequest.x + addRequest.y)
+    return AddResponse(addRequest.x + addRequest.y + const)
   }
 }
